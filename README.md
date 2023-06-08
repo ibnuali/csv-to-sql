44b5d964973d676e4a41dffe15151f7cbc66608d
Ibnu Ali <ibnuali@gmail.com>
2023-06-09 06:01:25 +0700

    init project

---
+++ README.md
@@ -0,0 +1,12 @@
+
+# Description
+This program take a CSV file and convert it into a SQL file with INSERT statements, the CSV file must have a header row with the column names. file CSV must be in same folder, after success convert SQL file will be created in the same folder.
+
+
+
+## How to Run
+- clone this repository
+- npm install
+- open command and type csv-parser.js --table-name yourtablename
+
+
---
+++ csv-parser.js
@@ -0,0 +1,61 @@
+const fs = require("fs");
+const { parse } = require("csv-parse");
+
+const args = process.argv.slice(2);
+const tableNameArgIndex = args.findIndex((arg) => arg === "--table_name");
+
+if (tableNameArgIndex === -1 || tableNameArgIndex === args.length - 1) {
+  console.error("Please provide the --table_name argument.");
+  return;
+}
+
+const tableName = args[tableNameArgIndex + 1];
+
+const csvFilePath = `./${tableName}.csv`;
+
+if (fs.existsSync(`${tableName}.sql`)) {
+  fs.unlinkSync(`${tableName}.sql`);
+}
+
+fs.readFile(csvFilePath, "utf8", (err, data) => {
+  if (err) {
+    console.error("Error reading the CSV file:", err);
+    return;
+  }
+
+  parse(data, { cast: true }, (parseErr, csvRows) => {
+    if (parseErr) {
+      console.error("Error parsing the CSV:", parseErr);
+      return;
+    }
+
+    const columnHeaders = csvRows[0];
+    const dataRows = csvRows.slice(1);
+
+    const insertStatements = dataRows.map((rowData) => {
+      const values = rowData.map((value) => {
+        if (value === null) return "NULL";
+        if (typeof value === "number") return value;
+        const escapedValue = value.replace(/'/g, "''");
+        return `'${escapedValue}'`;
+      });
+
+      return `INSERT INTO ${tableName} (${columnHeaders.join(
+        ", "
+      )}) VALUES (${values.join(", ")});`;
+    });
+
+    const lastStatementIndex = insertStatements.length - 1;
+    insertStatements[lastStatementIndex] = insertStatements[
+      lastStatementIndex
+    ].replace(/,(\s*\))/, "$1");
+
+    fs.appendFile(
+      `${tableName}.sql`,
+      insertStatements.join("\n"),
+      function (err) {
+        if (err) throw err;
+      }
+    );
+  });
+});
